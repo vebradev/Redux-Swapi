38797975158a86094bbc5f269a65246c2b5963c9
vebradev <justinas@vebra.dev>
2019-06-19 15:14:07 +0300

    Fix imports & createStore funcion.

--- src/index.js
+++ src/index.js
@@ -4,21 +4,15 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import rootReducer from "./reducers";
 import { applyMiddleware } from "redux";
+import { compose } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import { rootReducer } from "./reducers"
-
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import rootReducer from "./reducers"
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  compose(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
