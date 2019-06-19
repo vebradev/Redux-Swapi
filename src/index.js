a76aa77ec9fcb2e7f5331026884a61890c9444dd
vebradev <justinas@vebra.dev>
2019-06-19 16:19:07 +0300

    Cleanup & fix createStore.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { applyMiddleware } from "redux";
-import { compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers"
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk, logger)),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
