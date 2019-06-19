7a26fb08357824ce8cc453cbfbe75e3857534548
vebradev <justinas@vebra.dev>
2019-06-19 14:40:22 +0300

    Import dependencies to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { rootReducer } from "./reducers"
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
